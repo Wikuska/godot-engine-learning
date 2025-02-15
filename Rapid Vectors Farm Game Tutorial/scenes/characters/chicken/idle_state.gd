e7241e9b904803a6a6532d8c4424d853e6dec7f8
Wiktoria <wiktoriabanach@op.pl>
2025-02-15 20:04:47 +0100

    Create idle state functionality
    Create idle_state_timeout variable which will tell if timeout happened or not yet
    
    On ready set timer wait time and connect timeout to on_timeout function which will set idle_state_timeout variable to true
    
    On next transition check if timeout happened, if yes, emit transition signal
    
    On enter play idle animation, set idle_state_timeout variable to false and start timer
    
    On exit stop animation and timer

--- Rapid Vectors Farm Game Tutorial/scenes/characters/chicken/idle_state.gd
+++ Rapid Vectors Farm Game Tutorial/scenes/characters/chicken/idle_state.gd
@@ -2,6 +2,17 @@ extends NodeState
 
 @export var character: CharacterBody2D
 @export var animated_sprite_2d: AnimatedSprite2D
+@export var idle_state_time_interval: float = 5.0
+
+@onready var idle_state_timer: Timer = Timer.new()
+
+var idle_state_timeout: bool = false
+
+func _ready() -> void:
+	idle_state_timer.wait_time = idle_state_time_interval
+	idle_state_timer.timeout.connect(on_idle_state_timeout)
+	add_child(idle_state_timer)
+
 
 func _on_process(_delta : float) -> void:
 	pass
@@ -12,12 +23,20 @@ func _on_physics_process(_delta : float) -> void:
 
 
 func _on_next_transitions() -> void:
-	pass
+	if idle_state_timeout:
+		transition.emit("Walk")
 
 
 func _on_enter() -> void:
-	pass
+	animated_sprite_2d.play("idle")
+	idle_state_timeout = false
+	idle_state_timer.start()
 
 
 func _on_exit() -> void:
-	pass
+	animated_sprite_2d.stop()
+	idle_state_timer.stop()
+
+
+func on_idle_state_timeout() -> void:
+	idle_state_timeout = true
